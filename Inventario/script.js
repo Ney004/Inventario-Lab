514c32380fff3c95d235c64ce15ac41b69bd91b7
Ney Luis <167217593+Ney004@users.noreply.github.com>
2025-02-27 21:37:32 -0500

    Delete Inventario directory

--- Inventario/script.js
+++
@@ -1,6 +0,0 @@
-function handleCredentialResponse(response) {
-    console.log("ID Token:", response.credential);
-    // Redirigir al usuario al index.html
-    window.location.href = "index.html";
-}
-
